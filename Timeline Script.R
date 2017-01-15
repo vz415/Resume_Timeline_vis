e99ed6546fc21ae8e980c979037b81e9af648c07
vz415 <vdzaball@gmail.com>
2017-01-15 00:24:18 -0700

    Add files via upload

---
+++ Timeline Script.R
@@ -0,0 +1,15 @@
+# Timeline Visualization of the Resume
+git remote add origin
+https://github.com/vz415/resume_timeline_vis.git
+git config remote.origin.url
+git@github.com:vz415/resume_timeline_vis.git
+git pull -u origin master
+git push -u origin master
+
+
+git remote add origin
+https://github.com/vz415/resume_timeline_vis.git
+git config remote.origin.url
+git@github.com:vz415/resume_timeline_vis.git
+git pull -u origin master
+git push -u origin master
